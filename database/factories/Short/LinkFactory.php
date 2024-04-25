4fc5b161fd92ecf9ff1c52bd54cb8ed7cbec1907
lemadior <lemadioro@gmail.com>
2024-04-25 11:00:37 +0300

    Refactoring and cleanup

--- database/factories/Short/LinkFactory.php
+++ database/factories/Short/LinkFactory.php
@@ -2,10 +2,11 @@
 
 namespace Database\Factories\Short;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends Factory<Model>
  */
 class LinkFactory extends Factory
 {
